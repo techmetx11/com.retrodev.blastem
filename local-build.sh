bb9fbdce8a0d0bd107355a4b26af863f44572f6a
Bastien Nocera <hadess@hadess.net>
2026-08-16 15:30:15 +0200

    Switch local-build script to use the test branch
    Like test builds on Flathub's vorarbeiter do.

--- local-build.sh
+++ local-build.sh
@@ -4,6 +4,8 @@ rm -f com.retrodev.blastem.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean _build com.retrodev.blastem.yaml --repo=_repo
-flatpak build-bundle _repo com.retrodev.blastem.flatpak com.retrodev.blastem master
+BRANCH=test
+
+flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build com.retrodev.blastem.yaml --repo=_repo
+flatpak build-bundle _repo com.retrodev.blastem.flatpak com.retrodev.blastem $BRANCH
 
